0c5e6d393ddf3010885c71d6433bd4064a93c883
Daniel Phillimore <dan@ovms.co>
2019-07-10 22:28:43 +0100

    Add docblock

--- src/builtin/functions/optionsAndInfo/extension.js
+++ src/builtin/functions/optionsAndInfo/extension.js
@@ -13,6 +13,14 @@ module.exports = function (internals) {
     var valueFactory = internals.valueFactory;
 
     return {
+        /**
+         * Fetches the names of all extension modules that have been loaded
+         *
+         * @see {@link https://secure.php.net/manual/en/function.get-loaded-extensions.php}
+         *
+         * @param {Reference|Value|Variable|null} onlyZendExtensions
+         * @return {ArrayValue}
+         */
         'get_loaded_extensions': function (/* onlyZendExtensions */) {
             return valueFactory.createArray([]);
         }
