b439ef29d07de3d72a9dde2551a1fa369e9309e9
Daniel Phillimore <dan@ovms.co>
2019-08-21 18:16:04 +0100

    Remove incomplete builtin FS PHP functions (for now)

--- src/builtin/functions/filesystem.js
+++ src/builtin/functions/filesystem.js
@@ -9,8 +9,7 @@
 
 'use strict';
 
-var _ = require('microdash'),
-    INCLUDE_PATH_INI = 'include_path',
+var INCLUDE_PATH_INI = 'include_path',
     PHPError = require('phpcommon').PHPError;
 
 module.exports = function (internals) {
@@ -68,70 +67,6 @@ module.exports = function (internals) {
 
             return valueFactory.createBoolean(fileSystem.isFile(path) || fileSystem.isDirectory(path));
         },
-        'file_put_contents': function (pathReference, dataReference, flagsReference, contextReference) {
-            var contextValue,
-                data,
-                dataValue,
-                fileSystem,
-                flagsValue,
-                pathValue,
-                stream,
-                values;
-
-            pathValue = pathReference.getValue();
-            dataValue = dataReference.getValue();
-            flagsValue = flagsReference ? (
-                flagsReference.getValue()
-            ) : null;
-            contextValue = contextReference ? (
-                contextReference.getValue()
-            ) : null;
-
-            // Handle `path` arg
-            if (pathValue.getType() !== 'string') {
-                callStack.raiseError(
-                    PHPError.E_WARNING,
-                    'file_exists() fixme'
-                );
-                return valueFactory.createBoolean(false);
-            }
-
-            // Handle `data` arg
-            if (dataValue.getType() === 'string') {
-                data = dataValue.getNative();
-            } else if (dataValue.getType() === 'array') {
-                values = dataValue.getValues();
-
-                _.each(values, function (value, key) {
-                    values[key] = value.coerceToString().getNative();
-                });
-
-                data = valueFactory.createString(values.join(''));
-            } else if (dataValue.getType() === 'resource') {
-                throw new Error('file_put_contents() :: Resource as data is not yet implemented');
-            } else {
-                throw new Error('fixme');
-            }
-
-            // Handle `flags` arg
-            if (flagsValue && (flagsValue.getType() !== 'integer' || flagsValue.getNative() !== 0)) {
-                throw new Error('file_put_contents() :: Only flags=0 is supported');
-            }
-
-            if (contextValue) {
-                throw new Error('file_put_contents() :: Custom contexts are not yet implemented');
-            }
-
-            fileSystem = getFileSystem();
-
-            // Open the file, write its new contents to it and close
-            stream = fileSystem.openSync(pathValue.getNative());
-            stream.writeSync(data);
-            stream.closeSync();
-
-            // Return the number of bytes written to the file on success
-            return valueFactory.createInteger(data.length);
-        },
 
         /**
          * Determines whether a file (not a directory) exists with the given path
@@ -168,10 +103,6 @@ module.exports = function (internals) {
 
             return valueFactory.createBoolean(fileSystem.isFile(path));
         },
-        'realpath': function (pathReference) {
-            // FIXME: Stub
-            return pathReference;
-        },
 
         /**
          * Changes the include path, returning the old one
@@ -186,22 +117,6 @@ module.exports = function (internals) {
             iniState.set(INCLUDE_PATH_INI, newIncludePathReference.getValue().getNative());
 
             return valueFactory.createString(oldIncludePath);
-        },
-
-        'unlink': function (pathReference, contextReference) {
-            var fileSystem,
-                path;
-
-            if (contextReference) {
-                throw new Error('unlink() :: Context arg not yet supported');
-            }
-
-            fileSystem = getFileSystem();
-            path = pathReference.getValue().getNative();
-
-            fileSystem.unlinkSync(path);
-
-            return valueFactory.createBoolean(true);
         }
     };
 };
